3af59d489da567f829573dfbecc096f5c99aae73
AceAltair13 <tirththoria13@hotmail.com>
2021-08-28 22:07:00 +0530

    Updates

--- src/components/exam/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
-
-function Test() {
-    return (<Typography variant="h2" color="primary">Examination Page</Typography>);
-}
-
-export default Test;
