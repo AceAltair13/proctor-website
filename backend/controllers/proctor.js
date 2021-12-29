3f9609e91531df62dd3848f04496804a5eea37f0
varun172121 <nuravkumar1721212@gmail.com>
2021-12-29 12:50:51 +0530

    changes

--- backend/controllers/proctor.js
+++ backend/controllers/proctor.js
@@ -1,4 +1,4 @@
-import { storageRef } from "../db.js"
+import { storageRef, firebase_firestore } from "../db.js"
 import { examAccess } from "../helpers/exams.js"
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import fs from "fs"
@@ -8,25 +8,34 @@ const postMalpractice = async (req, res) => {
     const folder = req.user.userId + "/" + req.file.filename
     const storageRef_1 = ref(storageRef, folder);
     fs.readFile(req.file.path, function (err, buffer) {
-        uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
-            console.log('file is uploaded successfully!');
-        });
+        upload(buffer)
     })
-
-    await getDownloadURL(ref(storageRef_1))
-        .then((url) => {
-            console.log("download url", url)
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-
-    res.status(200).json("file is uploaded successfully")
+    download()
     fs.unlink(req.file.path, (err) => {
         if (err) throw err;
         console.log('done');
     });
+    res.status(200).json("file is uploaded successfully")
     res.end()
+    async function download() {
+        const pointer = JSON.stringify(ref(storageRef_1));
+        console.log(typeof (pointer))
+        await firebase_firestore.collection("users").doc(req.user.userId).update({ ref: pointer });
+        console.log(storageRef_1)
+        await getDownloadURL(ref(storageRef_1))
+            .then((url) => {
+                console.log("download url", url);
+            })
+            .catch((error) => {
+                console.log("errr", error);
+            });
+    }
+
+    async function upload(buffer) {
+        await uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
+            console.log('file is uploaded successfully!');
+        });
+    }
     // try {
     //     exam = await firebase_firestore.collection("users").doc(req.user.userId).get()
     // } catch (error) {
