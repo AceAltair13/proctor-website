6f268598b7ffc1e3340dcaa541195e981ade270c
AceAltair13 <tirththoria13@hotmail.com>
2022-01-08 21:27:28 +0530

    fix null token issue in axios create

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,23 +1,44 @@
 import axios from "axios";
 import { URL } from "./Constants/urls";
 
-const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser?.accessToken;
+export const setToken = (token) => {
+    axios.defaults.headers.common["token"] = token;
+    localStorage.setItem("token", token);
+};
 
-export const publicRequest = axios.create({
+export const getToken = () => {
+    return localStorage.getItem("token");
+};
+
+export const removeToken = () => {
+    localStorage.removeItem("token");
+};
+
+const publicRequest = axios.create({
     baseURL: URL,
 });
 
-export const userRequest = axios.create({
+const userRequest = axios.create({
     baseURL: URL,
-    headers: { token: TOKEN },
+    headers: { token: getToken() },
 });
 
-export const postImageRequest = axios.create({
-    baseURL: URL,
-    headers: {
-        token: TOKEN,
-        // examId:
+userRequest.interceptors.request.use(
+    (config) => {
+        config.headers.token = getToken();
+        return config;
     },
-});
+    (error) => {
+        console.log(error);
+    }
+);
+
+export { userRequest, publicRequest };
+
+// export const postImageRequest = axios.create({
+//     baseURL: URL,
+//     headers: {
+//         token: getToken(),
+//         // examId:
+//     },
+// });
