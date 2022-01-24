156a5312e66709821c3f64e41159fd172926acea
varun172121 <nuravkumar1721212@gmail.com>
2022-01-24 12:47:06 +0530

    direct string upload

--- backend/controllers/proctor.js
+++ backend/controllers/proctor.js
@@ -1,30 +1,36 @@
 import { storageRef, firebase_firestore } from "../db.js"
 import { uid } from "../helpers/other.js";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import fs from "fs"
-import base64ToImage from "base64-to-image";
-import path from "path";
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
+// import fs from "fs"
+// import base64ToImage from "base64-to-image";
+// import path from "path";
 
 const upload_face = async (req, res) => {
-    const __dirname = path.resolve();
-    var optionalObj = { 'fileName': 'face', 'type': 'jpeg' };
-    base64ToImage(req.body.image, path.join(__dirname, "controllers"), optionalObj)
+    // console.log(req.body.image)
+    // const __dirname = path.resolve();
+    // var optionalObj = { 'fileName': 'face', 'type': 'jpeg' };
+    // base64ToImage(req.body.image, path.join(__dirname, "controllers"), optionalObj)
     const metadata = { contentType: 'image/jpeg; charset=utf-8' }
     const folder = req.user.userId + req.body.exam + "/" + "faceRecoginition" + "/" + "face.jpeg"
     const storageRef_1 = ref(storageRef, folder);
-    fs.readFile(path.join(__dirname) + "/controllersface.jpeg", function (err, buffer) {
-        upload(buffer)
-    })
-    fs.unlink(path.join(__dirname) + "/controllersface.jpeg", (err) => {
-        if (err) throw err;
-        console.log('done');
-    });
+    upload()
+    // fs.readFile(path.join(__dirname) + "/controllersface.jpeg", function (err, buffer) {
+    //     upload()
+    // })
+    // fs.unlink(path.join(__dirname) + "/controllersface.jpeg", (err) => {
+    //     if (err) throw err;
+    //     console.log('done');
+    // });
     res.status(200).json("file is uploaded successfully")
     res.end()
-    async function upload(buffer) {
-        await uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
-            console.log('file is uploaded Successfully!');
+    async function upload() {
+        // await uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
+        //     console.log('file is uploaded Successfully!');
+        // });
+        uploadString(storageRef_1, req.body.image, 'data_url', metadata).then((snapshot) => {
+            console.log('Uploaded a base64 string!', snapshot);
         });
+
     }
 
 
@@ -43,28 +49,32 @@ const upload_face = async (req, res) => {
                 console.log("errr", error);
             });
     }
-    setTimeout(download_link, 2000)
+    setTimeout(download_link, 300)
 }
 const malpractices = async (req, res) => {
     const newId = uid();
-    const __dirname = path.resolve();
-    var optionalObj = { 'fileName': newId, 'type': 'jpeg' };
-    base64ToImage(req.body.image, path.join(__dirname, "controllers"), optionalObj)
+    // const __dirname = path.resolve();
+    // var optionalObj = { 'fileName': newId, 'type': 'jpeg' };
+    // base64ToImage(req.body.image, path.join(__dirname, "controllers"), optionalObj)
     const metadata = { contentType: 'image/jpeg; charset=utf-8' }
     const folder = req.user.userId + req.body.exam + "/" + req.body.type + "/" + newId + ".jpeg"
     const storageRef_1 = ref(storageRef, folder);
-    fs.readFile(path.join(__dirname) + "/controllers" + newId + ".jpeg", function (err, buffer) {
-        upload(buffer)
-    })
-    fs.unlink(path.join(__dirname) + "/controllers" + newId + ".jpeg", (err) => {
-        if (err) throw err;
-        console.log('done');
-    });
+    upload()
+    // fs.readFile(path.join(__dirname) + "/controllers" + newId + ".jpeg", function (err, buffer) {
+    //     upload(buffer)
+    // })
+    // fs.unlink(path.join(__dirname) + "/controllers" + newId + ".jpeg", (err) => {
+    //     if (err) throw err;
+    //     console.log('done');
+    // });
     res.status(200).json("file is uploaded successfully")
     res.end()
-    async function upload(buffer) {
-        await uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
-            console.log('file is uploaded Successfully!');
+    async function upload() {
+        // await uploadBytes(storageRef_1, buffer, metadata).then((snapshot) => {
+        //     console.log('file is uploaded Successfully!');
+        // });
+        uploadString(storageRef_1, req.body.image, 'data_url', metadata).then((snapshot) => {
+            console.log('Uploaded a base64 string!', snapshot);
         });
     }
 
@@ -84,7 +94,7 @@ const malpractices = async (req, res) => {
                 console.log("errr", error);
             });
     }
-    setTimeout(download_link, 2000)
+    setTimeout(download_link, 300)
 }
 export {
     upload_face, malpractices
